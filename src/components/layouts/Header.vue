c9cd85c05aba63af1650a0961bca2115e76c2a68
AleXandra Rudivickaya <rag@one-touch.ru>
2020-10-05 10:28:16 +0300

    header

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,11 +1,37 @@
 <template>
     <header class="header">
-        <router-link :to="{name: 'Home'}">
-            Главная
-        </router-link>
-        <router-link :to="{name: 'Projects'}">
-            Проекты
-        </router-link>
+        <div class="container-fluid">
+            <div class="row justify-content-between align-items-center">
+                <router-link
+                        class="header__logo"
+                        :to="{name: 'Home'}">
+                    <img src="@/assets/img/logo.svg">
+                </router-link>
+                <button class="header__toggle">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+                <nav class="header__menu-wrap">
+                    <ul class="header__menu">
+                        <li class="header__menu-item">
+                            <router-link
+                                    class="header__menu-link"
+                                    :to="{name: 'Home'}">
+                                Главная
+                            </router-link>
+                        </li>
+                        <li class="header__menu-item">
+                            <router-link
+                                    class="header__menu-link"
+                                    :to="{name: 'Projects'}">
+                                Проекты
+                            </router-link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
     </header>
 </template>
 
@@ -16,5 +42,43 @@
 </script>
 
 <style lang="scss">
+    .header {
+        padding: 50px;
 
+        &__toggle {
+            position: relative;
+            border: none;
+            background-color: transparent;
+            width: 30px;
+            height: 20px;
+
+            span {
+                position: absolute;
+                height: 4px;
+                border-radius: 4px;
+                width: 100%;
+                left: 0;
+                right: 0;
+                background-color: $accent-color;
+
+                &:first-child {
+                    top: 0;
+                }
+
+                &:nth-child(2) {
+                    top: calc(50% - 2px);
+                }
+
+                &:last-child {
+                    bottom: 0;
+                }
+            }
+        }
+
+        &__menu-wrap {
+            position: fixed;
+            right: -100%;
+            width: 100%;
+        }
+    }
 </style>
