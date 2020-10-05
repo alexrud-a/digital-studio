26231f27e2de21f5d8f8da3f1e5b93806cde79b9
AleXandra Rudivickaya <rag@one-touch.ru>
2020-10-05 15:42:15 +0300

    banner

--- src/components/projects/Projects.vue
+++ src/components/projects/Projects.vue
@@ -1,12 +1,27 @@
 <template>
     <div class="projects">
-        <h1>Projects</h1>
+        <Banner :banner="banner"/>
     </div>
 </template>
 
 <script>
+    import Banner from "../layouts/Banner";
     export default {
-        name: "Projects"
+        name: "Projects",
+        data() {
+            return {
+                banner: {
+                    title: 'Наши проекты',
+                    subtitle: 'Клиенты и проекты',
+                    bgVideo1: 'web-bg.mp4',
+                    bgVideo2: 'web-bg.webm',
+                    bgImg: 'web-bg.jpg',
+                }
+            }
+        },
+        components: {
+            Banner,
+        }
     }
 </script>
 
