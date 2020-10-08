070ac049d2613fc53a2184b22a2a4df4a9aa2df5
AleXandra Rudivickaya <rag@one-touch.ru>
2020-10-09 02:07:54 +0300

    footer

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -1,15 +1,29 @@
 <template>
     <footer class="footer">
-
+        <div class="container">
+            <div class="copyright">
+                Все права защищены © {{getYear}} <router-link :to="{name: 'Home'}" class="link">Digital-studio</router-link>
+            </div>
+        </div>
     </footer>
 </template>
 
 <script>
     export default {
-        name: "Footer"
+        name: "Footer",
+        data() {
+            return {
+                getYear: new Date().getFullYear()
+            }
+        }
     }
 </script>
 
 <style lang="scss">
-
+    .footer {
+        background-color: #000;
+        color: #fff;
+        padding: 60px 40px;
+        text-align: center;
+    }
 </style>
